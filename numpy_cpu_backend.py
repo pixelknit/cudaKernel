d1e6a2860a1961832d5a70c1129aa540d419ebb9
fpesantez <54825485+pixelknit@users.noreply.github.com>
2024-03-13 21:48:02 +0000

    Update numpy_cpu_backend.py

--- numpy_cpu_backend.py
+++ numpy_cpu_backend.py
@@ -1,36 +1,47 @@
 import numpy as np
+import random
 import time
 import colorama
 from colorama import Fore, Back, Style
 colorama.init(autoreset=True)
 
 def mm(matrix1,matrix2):
-
-    rows_matrix1, cols_matrix1 = matrix1.shape
-    rows_matrix2, cols_matrix2 = matrix2.shape
-
-    if cols_matrix1 != rows_matrix2:
-        print("Error! incompatible matrix shapes")
-        return
-
-    result = np.zeros((rows_matrix1, cols_matrix2)).astype(np.float32)
-
-    for i in range(rows_matrix1):
-        for j in range(cols_matrix2):
-            for k in range(cols_matrix1):
-                result[i,j] += matrix1[i,k] * matrix2[k,j]
-
+    result = []
+    for i, row in enumerate(matrix1):
+        newRow = []
+        for j, row in enumerate(row):
+            dot_product = sum(matrix1[i][k] * matrix2[k][j] for k in range(len(matrix2)))
+            newRow.append(dot_product)
+        result.append(newRow)
 
     return result
 
+def create_matrix(n):
+    return [[random.random() for _ in range(n)] for _ in range(n)]
+
+
+matrix1 = np.array([[2,4,5,1],
+                    [2,1,2,2],
+                    [8,1,2,3],
+                    [1,3,2,8]
+                    ])
+
+matrix2 = np.array([[2,4,5,1],
+                    [2,1,2,2],
+                    [8,1,2,3],
+                    [1,3,2,8]
+                    ])
+
 c_time = time.monotonic()
-N = 256
-A = np.random.rand(N,N).astype(np.float32)
-B = np.random.rand(N,N).astype(np.float32)
+N = 1024
+#A = np.random.rand(N,N).astype(np.float32)
+#B = np.random.rand(N,N).astype(np.float32)
 
-C = mm(A,B)
-e_time = time.monotonic()
+a = create_matrix(N)
+b = create_matrix(N)
 
+C = mm(a,b)
 print(C)
+e_time = time.monotonic()
 print("----------------")
 print(f"{Fore.YELLOW}Time: {e_time - c_time}")
